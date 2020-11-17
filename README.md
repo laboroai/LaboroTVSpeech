fa20b41f5d45e183e983dd75eb349560df6e1e20
s-and-o <ando@laboro.ai>
2020-11-17 17:05:19 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,104 @@
+# LaboroTVSpeech
+
+A large-scale Japanese speech corpus on TV recordings
+
+## About the Corpus
+
+LaboroTVSpeech is a large-scale Japanese speech corpus built from broadcast TV recordings and their subtitles.
+Our current release contains over 2,000 hours of speech.
+
+### Details
+
+- All audio samples were segmented based on the boudaries of the original subtitles, with sampling rates of 16 KHz.
+
+- Each speech semgments are tokenized into sequence of words using [MeCab](https://taku910.github.io/mecab/) with [mecab-ipadic-NEologd](https://github.com/neologd/mecab-ipadic-neologd) as a dictionary.
+- Each word token contains a simple morpheme tag such as 名詞 (noun) or 動詞 (verb), obatained through preprocessing the original subtitles.
+- From the original TV audio and subtitles, we have extracted the speech segments where we were able to align the audio and subtitle segment with high confidence.
+  - We iteraively used [segment_long_utterances_nnet3.sh](https://github.com/kaldi-asr/kaldi/blob/master/egs/wsj/s5/steps/cleanup/segment_long_utterances_nnet3.sh) and [clean_and_segment_data_nnet3.sh](https://github.com/kaldi-asr/kaldi/blob/master/egs/wsj/s5/steps/cleanup/clean_and_segment_data_nnet3.sh).
+- All speech segments are randomly shuffled.
+
+### Subsets
+
+|                      | train  | dev   |
+| -------------------- | ------ | ----- |
+| Audio length (hours) | 2036.2 | 13.7  |
+| # Audio segments     | 1.6 M  | 12 K  |
+| # Words (tokens)     | 22 M   | 147 K |
+
+### Precaution
+
+- Some words' pronunciation or morpheme tag may be incorrect, especially for casual words.
+  - e.g. 「すげえ」 (_Sugē_, casual form of _Sugoi_=great, very, etc.) → 「すげ+動詞 え+フィラー」(tokenized as verb + filler)
+- Each speech segment in this dataset **does not** have a speaker tag. You have to treat each segment as an utterance spoken by a unique speaker.
+- **We do not provide a test set** in this corpus, because it was nearly imporssible not to make speakers in the test set appear in the training or development set considering that many celebrities appear in several programs.
+  - In stead of the test set of LaboroTVSpeech, you may use [TEDxJP-10K](#tedxjp-10k-dataset).
+
+## Download
+
+N/A
+
+## Recipe for Kaldi Speech Recognition Tooklit
+
+### How to use
+
+We have evaluated LaboroTVSpeech by building an ASR model using the [Kaldi Speech Recognition Toolkit](https://github.com/kaldi-asr/kaldi).
+The recipe is based on Kaldi's [official CSJ recipe](https://github.com/kaldi-asr/kaldi/tree/master/egs/csj/s5).
+Copy `kaldi/laboro_tv_speech` to your `$KALDI_ROOT/egs` and execute `s5/run.sh` to train an ASR system from the beggining.
+
+### Optional Arguments for `run.sh`
+
+Without any optional arguments, the training and testing use LaboroTVSpeech only.
+There are several optional arguments you can set to use additional dataset during testing:
+
+- `--include-tedx true`
+
+  - With this option, we will also test the ASR model using TEDxJP-10K datast.
+  - This option supposes the original wav files and subtitle files already exist in `data/local/tedxjp`.
+  - Read [TEDxJP-10K dataset](#tedxjp-10k-dataset) for details.
+
+- `--include-oscar-lm true`
+
+  - With this option, we will also use a LM built from OSCAR corpus.
+  <!-- - This option supposes 3-gram count file of OSCAR already exists in `data/local/oscar.gz`. -->
+  - You have to copy an arpa-format LM (`oscar_200Kvocab_prune1e-8.o3g.kn.gz`) and lexicon.txt to `data/local/lm_oscar` beforehand.
+  - Read [OSCAR Language Model](#oscar-language-model) for details.
+
+- `--include-lm-interp true`
+  - With this option, the script builds TV+OSCAR-LM by interpolating the default TV-LM and OSCAR-LM.
+  - This option is allowed only if `--include-oscar-lm true` is set.
+
+## Optional Data
+
+### TEDxJP-10K Dataset
+
+This is a Japanese speech dataset built from Japanese TEDx videos and their subtitles.
+To build this dataset, you have to prepare audio and subtitle files beforehand.
+The original TEDx videos are listed [HERE](kaldi/laboro_tv_speech/s5/local/tedx-jp-10k.csv).
+For example, you can download an audio file (`<video-id>.wav`) and the corresponding subtitle file (`<video-id>.ja.vtt`) as follows, using `youtube-dl`:
+
+```bash
+youtube-dl \
+  --extract-audio \
+  --audio-format wav \
+  --write-sub \
+  --sub-format vtt \
+  --sub-lang ja \
+  --output "data/local/tedx-jp/%(id)s.%(ext)s" \
+  'https://www.youtube.com/watch?v=-6K2nN9aWsg'
+```
+
+#### Notes
+
+- The text are **NOT** tokenized by default.
+- This dataset contains manually annotated verbatim data, yet a few segments have incorrect timestamps or text.
+
+### OSCAR Language Model
+
+This LM was trained using the [OSCAR](https://oscar-corpus.com/) corpus containing 100 GB of web-crawled Japanese text. We estimated the pronunciations of the words in the lexicon in the same way as the LaboroTVSpeech. When building LMs, we have selected the vocabulary based on frequency counts.
+
+#### Download
+
+- [oscar_3gram_lm_v1.0.tar.gz]()
+  - 3-gram counts
+  - 3-gram LM with vocabulary size = 200K (pruned 1e-8)
+  - pronunciation dictionaries
---
+++ kaldi/laboro_tv_speech/s5/run.sh
@@ -0,0 +1,287 @@
+#!/usr/bin/env bash
+
+# Copyright  2020 Laboro.AI, Inc.
+#                 (Authors: Shintaro Ando and Hiromasa Fujihara)
+# Apache 2.0
+
+# This recipe is based on the two following recipes:
+# - We imported the hyperparameters and the training procedures from
+#   the CSJ recipe by Takafumi Moriya, Tomohiro Tanaka, Takahiro Shinozaki and Shinji Watanabe
+#   in the egs/csj/s5/ directory.
+# - We imported the `stage`s from
+#   the WSJ corpus recipe in the egs/wsj/s5/ directory.
+
+# This is a shell script, but it's recommended that you run the commands one by
+# one by copying and pasting into the shell.
+# Caution: some of the graph creation steps use quite a bit of memory, so you
+# should run this on a machine that has sufficient memory.
+
+. cmd.sh
+. path.sh
+set -e
+
+nj=50
+nj_decode=10
+
+stage=0
+dnn_stage=0
+
+dev_set="dev"
+test_sets=""
+test_lang_names="lang_laborotv_tg"
+oscar_lm_name="oscar_200Kvocab_prune1e-8"
+
+# options to include additional testing dataset & LMs
+include_tedx=false
+include_oscar_lm=false
+include_lm_interp=false
+
+. utils/parse_options.sh
+
+LABOROTV_DATA_ROOT="/mnt/data/LaboroTVSpeech_v1.0b"
+TEDXJP_DATA_ROOT="data/local/tedx-jp"
+
+# Handle options
+if ${include_tedx}; then
+  test_sets="tedx-jp-10k_verbatim"
+fi
+
+if ${include_oscar_lm}; then
+  test_lang_names="${test_lang_names} lang_nosp_${oscar_lm_name}_tg"
+fi
+
+if ${include_lm_interp}; then
+  if [[ ! ${include_oscar_lm} ]]; then
+    echo "$0: '--include-oscar true' should be set when '--include-lm-interp true'"
+  fi
+  test_lang_names="${test_lang_names} lang_nosp_tv-${oscar_lm_name}-interp_tg"
+fi
+
+if [[ ${stage} -le 0 ]]; then
+  # Prepare train & dev set
+  local/laborotv_data_prep.sh ${LABOROTV_DATA_ROOT}
+
+  if ${include_tedx}; then
+    # Prepare TEDx-JP-10K to use as eval set.
+    local/tedx-jp/10k_data_prep.sh ${TEDXJP_DATA_ROOT}
+  fi
+
+  local/lm/prepare_dict.sh \
+    ${LABOROTV_DATA_ROOT}/data/lexicon.txt data/local/dict_nosp
+
+  utils/prepare_lang.sh --num-sil-states 4 data/local/dict_nosp "<unk>" data/local/lang_nosp data/lang_nosp
+
+  # Now train the language models.
+  local/laborotv_train_lms.sh \
+    data/train/text data/local/dict_nosp/lexicon.txt data/local/lm
+
+  # We don't really need all these options for SRILM, since the LM training script
+  # does some of the same processing (e.g. -subset -tolower)
+  srilm_opts="-subset -prune-lowprobs -unk -tolower -order 3"
+  LM=data/local/lm/laborotv.o3g.kn.gz
+  utils/format_lm_sri.sh --srilm-opts "$srilm_opts" \
+    data/lang_nosp $LM data/local/dict_nosp/lexicon.txt data/lang_nosp_laborotv_tg
+
+  if ${include_oscar_lm}; then
+    local/lm/prepare_lang_from_arpa.sh \
+      --suffix "_${oscar_lm_name}" \
+      "data/local/lm_oscar/${oscar_lm_name}.o3g.kn.gz" \
+      "data/local/lm_oscar/lexicon.txt"
+  fi
+
+  if ${include_lm_interp}; then
+    local/lm/interpolate_best_mix.sh \
+      --dev-text "data/dev/text" \
+      --remove-utt-ids-from-dev-text true \
+      --src-lexicons "data/local/dict_nosp/lexicon.txt data/local/dict_nosp_${oscar_lm_name}/lexicon.txt" \
+      --src-lms "data/local/lm/laborotv.o3g.kn.gz data/local/lm_oscar/${oscar_lm_name}.o3g.kn.gz" \
+      "tv-${oscar_lm_name}-interp"
+  fi
+
+  for dir_name in train ${test_sets} ${dev_set}; do
+    steps/make_mfcc.sh --nj ${nj} data/${dir_name}
+    steps/compute_cmvn_stats.sh data/${dir_name}
+    utils/validate_data_dir.sh data/${dir_name}
+  done
+
+  # We create subsets for training in earlier stages
+  # in the same way as the CSJ recipe
+  utils/subset_data_dir.sh \
+    --shortest data/train \
+    100000 \
+    data/train_100kshort
+
+  utils/subset_data_dir.sh \
+    data/train_100kshort \
+    30000 \
+    data/train_30kshort
+
+  utils/subset_data_dir.sh \
+    --first \
+    data/train \
+    100000 \
+    data/train_100k
+fi
+
+if [[ ${stage} -le 1 ]]; then
+  # mono
+  steps/train_mono.sh --nj ${nj} --cmd "$train_cmd" \
+    data/train_30kshort \
+    data/lang_nosp \
+    exp/mono
+fi
+
+if [[ ${stage} -le 2 ]]; then
+  # tri1
+  steps/align_si.sh --nj ${nj} --cmd "$train_cmd" \
+    data/train_100k \
+    data/lang_nosp \
+    exp/mono \
+    exp/mono_ali
+
+  steps/train_deltas.sh --cmd "$train_cmd" \
+    3200 30000 \
+    data/train_100k \
+    data/lang_nosp \
+    exp/mono_ali \
+    exp/tri1
+
+  graph_dir=exp/tri1/graph_nosp_laborotv_tg
+  $train_cmd $graph_dir/mkgraph.log \
+    utils/mkgraph.sh data/lang_nosp_laborotv_tg exp/tri1 $graph_dir
+  for eval_id in ${test_sets} $dev_set; do
+    steps/decode_si.sh --nj ${nj_decode} --cmd "$decode_cmd" --config conf/decode.config \
+      $graph_dir data/$eval_id exp/tri1/decode_nosp_laborotv_${eval_id}
+  done
+fi
+
+if [[ ${stage} -le 3 ]]; then
+  # tri2
+  steps/align_si.sh --nj ${nj} --cmd "$train_cmd" \
+    data/train_100k \
+    data/lang_nosp \
+    exp/tri1 \
+    exp/tri1_ali
+
+  steps/train_deltas.sh --cmd "$train_cmd" \
+    4000 70000 \
+    data/train_100k \
+    data/lang_nosp \
+    exp/tri1_ali \
+    exp/tri2
+
+  # The previous mkgraph might be writing to this file.  If the previous mkgraph
+  # is not running, you can remove this loop and this mkgraph will create it.
+  while [ ! -s data/lang_nosp_laborotv_tg/tmp/CLG_3_1.fst ]; do sleep 60; done
+  sleep 20 # in case still writing.
+  graph_dir=exp/tri2/graph_nosp_laborotv_tg
+  $train_cmd $graph_dir/mkgraph.log \
+    utils/mkgraph.sh data/lang_nosp_laborotv_tg exp/tri2 $graph_dir
+  for eval_id in ${test_sets} $dev_set; do
+    steps/decode.sh --nj ${nj_decode} --cmd "$decode_cmd" --config conf/decode.config \
+      $graph_dir data/$eval_id exp/tri2/decode_nosp_laborotv_${eval_id}
+  done
+fi
+
+if [[ ${stage} -le 4 ]]; then
+  # tri3
+  # From now, we start with the LDA+MLLT system
+  steps/align_si.sh --nj ${nj} --cmd "$train_cmd" \
+    data/train_100k \
+    data/lang_nosp \
+    exp/tri2 \
+    exp/tri2_ali_100k
+
+  # From now, we start using all of the data (except some duplicates of common
+  # utterances, which don't really contribute much).
+  steps/align_si.sh --nj ${nj} --cmd "$train_cmd" \
+    data/train \
+    data/lang_nosp \
+    exp/tri2 \
+    exp/tri2_ali
+
+  # Do another iteration of LDA+MLLT training, on all the data.
+  steps/train_lda_mllt.sh \
+    --cmd "$train_cmd" \
+    6000 140000 \
+    data/train \
+    data/lang_nosp \
+    exp/tri2_ali \
+    exp/tri3
+
+  graph_dir=exp/tri3/graph_nosp_laborotv_tg
+  $train_cmd $graph_dir/mkgraph.log \
+    utils/mkgraph.sh data/lang_nosp_laborotv_tg exp/tri3 $graph_dir
+  for eval_id in ${test_sets} $dev_set; do
+    steps/decode.sh --nj ${nj_decode} --cmd "$decode_cmd" --config conf/decode.config \
+      $graph_dir data/$eval_id exp/tri3/decode_nosp_laborotv_${eval_id}
+  done
+fi
+
+if [[ ${stage} -le 5 ]]; then
+  # Now we compute the pronunciation and silence probabilities from training data,
+  # and re-create the lang directory.
+  steps/get_prons.sh --cmd "$train_cmd" \
+    data/train \
+    data/lang_nosp \
+    exp/tri3
+  utils/dict_dir_add_pronprobs.sh --max-normalize true \
+    data/local/dict_nosp exp/tri3/pron_counts_nowb.txt exp/tri3/sil_counts_nowb.txt \
+    exp/tri3/pron_bigram_counts_nowb.txt data/local/dict
+
+  utils/prepare_lang.sh data/local/dict "<unk>" data/local/lang data/lang
+  LM=data/local/lm/laborotv.o3g.kn.gz
+  srilm_opts="-subset -prune-lowprobs -unk -tolower -order 3"
+  utils/format_lm_sri.sh --srilm-opts "$srilm_opts" \
+    data/lang $LM data/local/dict/lexicon.txt data/lang_laborotv_tg
+
+  graph_dir=exp/tri3/graph_laborotv_tg
+  $train_cmd $graph_dir/mkgraph.log \
+    utils/mkgraph.sh data/lang_laborotv_tg exp/tri3 $graph_dir
+  for eval_id in ${test_sets} $dev_set; do
+    steps/decode.sh --nj ${nj_decode} --cmd "$decode_cmd" --config conf/decode.config \
+      $graph_dir data/$eval_id exp/tri3/decode_laborotv_tg_${eval_id}
+  done
+fi
+
+if [[ ${stage} -le 6 ]]; then
+  # Train tri4, which is LDA+MLLT+SAT, on all the data.
+  steps/align_fmllr.sh --nj ${nj} --cmd "$train_cmd" \
+    data/train \
+    data/lang \
+    exp/tri3 \
+    exp/tri3_ali
+
+  steps/train_sat.sh --cmd "$train_cmd" \
+    11500 200000 \
+    data/train \
+    data/lang \
+    exp/tri3_ali \
+    exp/tri4
+
+  graph_dir=exp/tri4/graph_laborotv_tg
+  $train_cmd $graph_dir/mkgraph.log \
+    utils/mkgraph.sh data/lang_laborotv_tg exp/tri4 $graph_dir
+  for eval_id in ${test_sets} $dev_set; do
+    steps/decode_fmllr.sh --nj ${nj_decode} --cmd "$decode_cmd" --config conf/decode.config \
+      $graph_dir data/${eval_id} exp/tri4/decode_laborotv_tg_${eval_id}
+  done
+fi
+
+if [[ ${stage} -le 7 ]]; then
+  # nnet3 TDNN+Chain
+  local/chain/run_tdnn.sh \
+    --test-sets "${test_sets}" \
+    --test-lang-names "${test_lang_names}" \
+    --stage ${dnn_stage}
+fi
+
+exit 0
+
+# getting results (see RESULTS file)
+# for eval_id in ${test_sets} $dev_set; do
+#   echo "=== evaluation set $eval_id ==="
+#   for x in exp/{tri,dnn}*/decode_*_${eval_id}*; do
+#     [ -d $x ] && grep WER $x/wer_* | utils/best_wer.sh
+#   done
+# done
